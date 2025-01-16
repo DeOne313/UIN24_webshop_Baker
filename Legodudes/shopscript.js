955c6f447c0ff75fbcd934bb871e46df0475b026
DeOne313 <mbalezai@hiof.no>
2025-01-16 08:30:54 +0100

    jhg

---
+++ Legodudes/shopscript.js
@@ -0,0 +1,16 @@
+console.log(products)
+
+//Gå gjennom alle produkter, generere  HTML for hvert produkt, skrive dette til index.html
+
+let productHTML = ""
+
+products.map((products, index) => productHTML += `<article class="product-card">
+                <img src="./website_images/PROD_${product.imagefile}" alt="PRODUKTTITTEL"/>
+                <a href="#KATEGORISIDE">Ninjago</a>
+                <h3>${product.title}</h3>
+                <p>kr. ${product.price},-</p>
+                <button>Legg til i handlekurv</button>
+            </article>`)
+
+//Finn #productlist, og fyll den med verdiene i variabelen productHTML.
+document.getElementById("productlist").innerHTML = productHTML
